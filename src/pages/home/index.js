05212e885504a290fe1b64f7186775b10d94e1f7
João Aires <airesjoao25@outlook.com>
2020-04-21 22:43:03 -0300

    init home layout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { ADD_PRODUCT } from '../../store/actions/actionTypes';
+import { FiHome, FiShoppingCart } from "react-icons/fi";
 
 // import { Container } from './styles';
 
@@ -10,8 +11,33 @@ export default function Home() {
 
   return (
     <div>
-        <h1>Home</h1>
-        <Link to="/cart">Cart</Link>
+
+        <div style={{
+          width: '100%',
+          borderBottom: '1px solid #eee'
+        }}>
+
+          <div style={{
+            height: 50,
+            maxWidth: 900,
+            margin: '0 auto',
+            display: 'flex', 
+            flexDirection: 'row', 
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: 10,
+          }}>
+            <div style={{ borderWidth: 1, borderColor: 'red' }}>
+              <FiHome size={30} />
+            </div>
+
+            <div>
+              <FiShoppingCart size={30} />
+            </div>
+          </div>
+
+        </div>
+        
     </div>
   );
 }
